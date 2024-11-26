51d10bd6e34f24eaef4aafaf197045d47551dc7c
stposey <110118637+stposey@users.noreply.github.com>
2024-11-26 16:48:48 -0500

    Update __init__.py
    Update Profits so they match

--- MyProject/FirmBehaviorSignal/__init__.py
+++ MyProject/FirmBehaviorSignal/__init__.py
@@ -275,7 +275,7 @@ def set_payoffs(group: Group):
     
     
     playerRank['demand']=[demand1,demand2,demand3,demand4,demand5]
-    playerRank['profit']= playerRank.price*playerRank.demand-(playerRank.quality+.5*playerRank.informalSignal+playerRank.formalSignal*10)*playerRank.demand
+    playerRank['profit']= 100*playerRank.price*playerRank.demand-(playerRank.quality+.5*playerRank.informalSignal+playerRank.formalSignal*10)*playerRank.demand
     playersDF= playerRank.sort_values(by = ['playerid'])
     playersDF.index=playersDF['playerid']
     
